37a7e080338c22c1fc86e8b21e98ab8a5f14309d
LiaoFeng <451118265@qq.com>
2019-08-21 11:17:28 +0800

    banner 测试

--- springwebflux/src/main/java/com/example/springwebflux/SpringwebfluxApplication.java
+++ springwebflux/src/main/java/com/example/springwebflux/SpringwebfluxApplication.java
@@ -1,5 +1,6 @@
 package com.example.springwebflux;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringwebfluxApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringwebfluxApplication.class, args);
+
+        SpringApplication application = new SpringApplication(SpringwebfluxApplication.class);
+        /*
+         * Banner.Mode.OFF:关闭;
+         * Banner.Mode.CONSOLE:控制台输出，默认方式;
+         * Banner.Mode.LOG:日志输出方式;
+         */
+        application.setBannerMode(Banner.Mode.CONSOLE );
+        application.run(args);
     }
 
 }
